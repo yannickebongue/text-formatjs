4ffad7130b818c3c7ead3b6063b9e6c9f88e0130
Yannick Ebongue <yannickebongue@yahoo.fr>
2017-01-10 22:16:00 +0100

    Use currency database file
    Use the currency database file to create clean currency objects from their currency code or by providing a locale with a valid country code.

--- src/currency.js
+++ src/currency.js
@@ -3,6 +3,7 @@
     if ( typeof module === "object" && typeof module.exports === "object" ) {
         module.require( "../resources/currency-names" );
         module.require( "./resource-bundle" );
+        module.require( "./currency-data" );
         module.require( "./locale" );
         module.exports = factory( global );
     } else {
@@ -39,19 +40,51 @@
         };
     }
 
-    var instances = {
-        "EUR": new Currency( "EUR", 2, 0 ),
-        "GBP": new Currency( "GBP", 2, 0 ),
-        "USD": new Currency( "USD", 2, 0 )
-    };
+    var instances = {};
 
-    Currency.getInstance = function( currencyCode ) {
-        var instance = instances[ currencyCode ];
-        if ( !instance ) {
-            instance = new Currency( currencyCode, -1, 0 );
-            instances[ currencyCode ] = instance;
+    Currency.getInstance = function( arg ) {
+        if ( typeof arg == "string" ) {
+            var currencyCode = arg.length > 0 ? arg : "XXX" ;
+            var instance = instances[ currencyCode ];
+            if ( !instance ) {
+                var data = global.CurrencyData;
+                var numericCode = 0;
+                var defaultFractionDigit = -1;
+                var result = new RegExp( currencyCode + "\\d{3}" ).exec( data["all"] ).toString();
+                if ( result && result.length > 0 ) {
+                    var regex = new RegExp( currencyCode );
+                    numericCode = result.substring( 3 );
+                    defaultFractionDigit = 2;
+                    if ( regex.test( data[ "minor0" ] ) ) {
+                        defaultFractionDigit = 0;
+                    }
+                    if ( regex.test( data[ "minor1" ] ) ) {
+                        defaultFractionDigit = 1;
+                    }
+                    if ( regex.test( data[ "minor3" ] ) ) {
+                        defaultFractionDigit = 3;
+                    }
+                    if ( regex.test( data[ "minorUndefined" ] ) ) {
+                        defaultFractionDigit = -1;
+                    }
+                }
+                instance = new Currency( currencyCode, defaultFractionDigit, numericCode );
+                if ( defaultFractionDigit != -1 ) {
+                    instances[ currencyCode ] = instance;
+                }
+            }
+            return instance;
+        } else if ( arg instanceof global.Locale ) {
+            var locale = arg;
+            var countryCode = locale.getCountry();
+            if ( countryCode && countryCode.length > 0 ) {
+                return Currency.getInstance( global.CurrencyData[ countryCode ] );
+            } else {
+                return new Currency( "XXX", -1, 0 );
+            }
+        } else {
+            return null;
         }
-        return instance;
     };
 
     Currency.prototype.toString = function() {
