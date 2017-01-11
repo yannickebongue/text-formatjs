fe8c254ccac27d1c5342eadffd8debe616b2176c
Yannick Ebongue <yannickebongue@yahoo.fr>
2017-01-11 11:36:54 +0100

    Define the `Format.Field` object

--- src/format.js
+++ src/format.js
@@ -10,9 +10,21 @@
 
     function Format() {}
 
-    Format.prototype.format = undefined;
+    Format.prototype.format = function( number ) {
+        return undefined;
+    };
 
-    Format.prototype.parse = undefined;
+    Format.prototype.parse = function( source ) {
+        return undefined;
+    };
+
+    Format.Field = function Field( name ) {
+        this.name = name;
+    };
+
+    Format.Field.prototype.toString = function() {
+        return this.constructor.name + "(" + this.name + ")";
+    };
 
     global.Format = Format;
 
