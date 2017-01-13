6cb6e86717a508a0eb1822be30df2f4b964ff573
Yannick Ebongue <yannickebongue@yahoo.fr>
2017-01-13 22:52:56 +0100

    Update base format data bundle

--- resources/format-data.js
+++ resources/format-data.js
@@ -10,15 +10,21 @@
 
     global.FormatData = {
         "": {
-            "MonthNames": [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "" ],
-            "MonthAbbreviations": [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "" ],
-            "DayNames": [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ],
+            "MonthNames": [ "January", "February", "March", "April", "May",
+                "June", "July", "August", "September",
+                "October", "November", "December", "" ],
+            "MonthAbbreviations": [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+                "Aug", "Sep", "Oct", "Nov", "Dec", "" ],
+            "DayNames": [ "Sunday", "Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday" ],
             "DayAbbreviations": [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ],
             "AmPmMarkers": [ "AM", "PM" ],
             "Eras": [ "BC", "AD" ],
-            "NumberPatterns": [ "#,##0.###;-#,##0.###", "\u00A4 #,##0.00;-\u00A4 #,##0.00", "#,##0%" ],
-            "NumberElements": [ ".", ",", ";", "%", "0", "#", "-", "E", "\u2030", "∞", "?" ],
-            "DateTimePatterns": [ "h:mm:ss a z", "h:mm:ss a z", "h:mm:ss a", "h:mm a", "EEEE, MMMM d, yyyy", "MMMM d, yyyy", "MMM d, yyyy", "M/d/yy", "{1} {0}" ],
+            "NumberPatterns": [ "#,##0.###;-#,##0.###", "¤ #,##0.00;-¤ #,##0.00", "#,##0%" ],
+            "NumberElements": [ ".", ",", ";", "%", "0", "#", "-", "E", "‰", "∞", "�" ],
+            "DateTimePatterns": [ "h:mm:ss a z", "h:mm:ss a z", "h:mm:ss a", "h:mm a",
+                "EEEE, MMMM d, yyyy", "MMMM d, yyyy",
+                "MMM d, yyyy", "M/d/yy", "{1} {0}" ],
             "DateTimePatternChars": "GyMdkHmsSEDFwWahKzZ"
         }
     };
