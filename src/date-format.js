ff34ae60814a3bccde90b71c082e5d3c5bbe176b
Yannick Ebongue <yannickebongue@yahoo.fr>
2017-01-17 13:49:56 +0100

    Add date format factory methods

--- src/date-format.js
+++ src/date-format.js
@@ -3,6 +3,7 @@
     if ( typeof module === "object" && typeof module.exports === "object" ) {
         module.require( "locale" );
         module.require( "format" );
+        module.require( "simple-date-format" );
         module.exports = factory( global );
     } else {
         factory( global );
@@ -13,6 +14,28 @@
     function DateFormat() {
         global.Format.call( this );
     }
+
+    var _get = function( timeStyle, dateStyle, flags, loc ) {
+        if ((flags & 1) != 0) {
+            if (timeStyle < 0 || timeStyle > 3) {
+                throw "Illegal time style " + timeStyle;
+            }
+        } else {
+            timeStyle = -1;
+        }
+        if ((flags & 2) != 0) {
+            if (dateStyle < 0 || dateStyle > 3) {
+                throw "Illegal date style " + dateStyle;
+            }
+        } else {
+            dateStyle = -1;
+        }
+        try {
+            return new global.SimpleDateFormat(timeStyle, dateStyle, loc);
+        } catch ( e ) {
+            return new SimpleDateFormat("M/d/yy h:mm a");
+        }
+    };
 
     DateFormat.ERA_FIELD = 0;
     DateFormat.YEAR_FIELD = 1;
@@ -38,6 +61,26 @@
     DateFormat.MEDIUM = 2;
     DateFormat.SHORT = 3;
     DateFormat.DEFAULT = DateFormat.MEDIUM;
+
+    DateFormat.getTimeInstance = function( style, locale ) {
+        return _get( ( typeof style != "undefined" ? style : DateFormat.DEFAULT ), 0,
+            1, locale || global.Locale.getDefault() );
+    };
+
+    DateFormat.getDateInstance = function( style, locale ) {
+        return _get( 0, ( typeof style != "undefined" ? style : DateFormat.DEFAULT ),
+            2, locale || global.Locale.getDefault() );
+    };
+
+    DateFormat.getDateTimeInstance = function( dateStyle, timeStyle, locale ) {
+        return _get( ( typeof timeStyle != "undefined" ? timeStyle : DateFormat.DEFAULT ),
+            ( typeof dateStyle != "undefined" ? dateStyle : DateFormat.DEFAULT ),
+            3, locale || global.Locale.getDefault() );
+    };
+
+    DateFormat.getInstance = function() {
+        return DateFormat.getDateTimeInstance( DateFormat.SHORT, DateFormat.SHORT );
+    };
 
     DateFormat.prototype = Object.create( global.Format.prototype );
 
