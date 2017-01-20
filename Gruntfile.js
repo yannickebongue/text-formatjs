2837b55f428ba56669bd6f0f8869b2ab2b8c5615
Yannick Ebongue <yannickebongue@yahoo.fr>
2017-01-20 15:01:13 +0100

    Add the Gruntfile.js to build the text-format library

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function( grunt ) {
+
+    "use strict";
+
+    var coreFiles = [
+        "resource-bundle.js",
+        "locale.js",
+        "currency-data.js",
+        "currency.js",
+        "field-position.js",
+        "parse-position.js",
+        "format.js"
+    ];
+
+    var numberFormatFiles = [
+        "number-format.js",
+        "digit-list.js",
+        "decimal-format-symbols.js",
+        "decimal-format.js",
+        "choice-format.js"
+    ];
+
+    var dateFormatFiles = [
+        "calendar-helper.js",
+        "date-format-symbols.js",
+        "date-format.js",
+        "simple-date-format.js"
+    ];
+
+    var sourceFiles = coreFiles.concat( numberFormatFiles ).concat( dateFormatFiles ).map( function( file ) {
+        return "src/" + file;
+    } );
+
+    var resourceDir = "resources/";
+    var resourceNames = [
+        "currency-names",
+        "format-data",
+        "locale-names"
+    ];
+
+    var resourceFiles = [];
+    resourceNames.forEach( function( baseName ) {
+        var pattern = resourceDir + baseName + "-*.js";
+        resourceFiles.push( resourceDir + baseName + ".js" );
+        resourceFiles = resourceFiles.concat( grunt.file.expandMapping( pattern ).map( function( item ) {
+            return item.dest;
+        } ) );
+    } );
+
+    grunt.initConfig( {
+
+        pkg: grunt.file.readJSON( "package.json" ),
+
+        clean: [ "dist/" ],
+
+        uglify: {
+            options: {
+                sourceMap: true,
+                banner: "/*! <%=pkg.name %> | <%= pkg.version %> | <%= grunt.template.today('yyyy-mm-dd') %> */\n"
+            },
+            js: {
+                options: {
+                    mangle: false,
+                    compress: false,
+                    beautify: true,
+                    sourceMap: false,
+                    preserveComments: "all"
+                },
+                files: {
+                    "dist/text-format.js": sourceFiles,
+                    "dist/text-resources.js": resourceFiles
+                }
+            },
+            main: {
+                files: {
+                    "dist/text-format.min.js": "dist/text-format.js"
+                }
+            },
+            resources: {
+                files: {
+                    "dist/text-resources.min.js": "dist/text-resources.js"
+                }
+            }
+        }
+
+    } );
+
+    grunt.loadNpmTasks( "grunt-contrib-clean" );
+    grunt.loadNpmTasks( "grunt-contrib-uglify" );
+
+    grunt.registerTask( "default", [ "clean", "uglify:js", "uglify:main", "uglify:resources" ] );
+
+};
